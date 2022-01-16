07ff19e43283227b14613b9235a438e137d2d2bb
Obodomechine Jake <jake@curacel.ai>
2022-01-16 11:52:24 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<!--[if lt IE 7 ]> <html lang="en" class="ie6">    <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7">    <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8">    <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9">    <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en"><!--<![endif]-->
+ 	
+<head>
+		<!-- META TAG -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->		
+		<!-- SITE TITLE -->
+	    <title>Jake's - Onepage CV/Resume Template</title>
+	    <!-- FAVICON -->
+	    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico"/>	
+		<!-- All CSS -->
+		<!-- GOOGLE FONT CSS -->
+		<link href="http://fonts.googleapis.com/css?family=Cousine:400,700|Montserrat:400,700&amp;subset=latin-ext" rel="stylesheet" type="text/css">
+		<!-- BOOTSTRAP CSS -->
+	    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	    <!-- FONT AWESOME CSS -->
+	    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">		
+		<!-- MAGNIFIC CSS -->
+		<link rel="stylesheet" href="assets/css/magnific-popup.css">
+		<!-- VENOBOX css -->
+	    <link rel="stylesheet" href="assets/css/venobox.css">		
+	    <!-- ANIMATE CSS -->
+	    <link rel="stylesheet" href="assets/css/animate.css">
+	    <!-- MAIN STYLE CSS -->
+	    <link rel="stylesheet" href="assets/css/style.css">
+		<!-- RESPONSIVE CSS -->
+	    <link rel="stylesheet" href="assets/css/responsive.css">
+		<!-- COLOR SWITCHER CSS -->
+		<link rel="stylesheet" href="assets/css/switcher/switcher.css"> 	
+		<link rel="stylesheet" href="assets/css/switcher/style1.css" id="colors">
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->		
+	</head>	
+  	<body>
+		<!-- START PRELOADER -->
+    	<div class="preloader"></div>
+		<!-- / END PRELOADER -->
+		
+		<!-- START HEADER DESIGN AREA -->
+		<header id="home" class="header-area">
+			<!-- START MENU DESIGN AREA -->
+			<div class="header-top-area">
+				<div class="container">
+					<div class="navbar navbar-default mainmenu">
+						<div class="navbar-header">
+							<button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="index.php">SOFIAT</a>
+						</div>
+						<div class="navbar-collapse collapse">
+							<nav>
+								<ul class="nav navbar-nav navbar-right">
+									<li class="active"><a class="smoth-scroll" href="#home">Home</a></li>
+									<li><a class="smoth-scroll" href="#about">About me</a></li>
+								</ul>
+							</nav>
+						</div> 
+					</div>
+				</div>
+			</div>
+			<!-- / END MENU DESIGN AREA -->
+			
+			<!-- START HEADER TEXT DESIGN AREA -->
+			<div class="header-bg header-img-one">
+				<div class="header-display-table">
+					<div class="header-display-table-cell">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="header-text">
+										<h3>Hello,<br />This is Sofiat Abayomi</h3>
+										<h2> I'm a Student</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- / END HEADER TEXT DESIGN AREA -->
+		</header>
+		<!-- / END HEADER DESIGN AREA -->
+		
+		<!-- START ABOUT ME DESIGN AREA -->
+		<section id="about" class="about-area section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 wow animated zoomIn">
+						<!-- START SECTION TITLE DESIGN AREA -->
+						<div class="section-title text-center">
+							<h2>About Me</h2>
+							<p>
+                                I am a dedicated and hard working students of Bells University of technology,who enjoys studying, and works hard to get good grades
+                            </p>
+						</div>
+						<!-- / END SECTION TITLE DESIGN AREA -->
+					</div>
+				</div>
+				<div class="row text-center">
+					<div class="col-md-4 col-sm-4 wow animated fadeIn" data-wow-delay="0.20s">
+						<div class="about-icon">
+							<i class="fa fa-bar-chart"></i>
+							<h4>Punctual</h4>
+							<p>A stitch in time saves nine</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 wow animated fadeIn" data-wow-delay="0.30s">
+						<div class="about-icon">
+							<i class="fa fa-area-chart"></i>
+							<h4>Creative</h4>
+							<p>The box gets congested,so i think out the box</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4 wow animated fadeIn" data-wow-delay="0.40s">
+						<div class="about-icon">
+							<i class="fa fa-line-chart"></i>
+							<h4>Motivated</h4>
+							<p>I enjoy what i do,so motivation is always available</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- START ABOUT SLIDE DESIGN AREA -->
+					<div class="col-md-4 col-sm-12 wow animated fadeInUp" data-wow-delay="0.50s">
+						<div class="about-slide">
+							<div id="about-carousel" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#about-carousel" data-slide-to="0" class="active"></li>
+									<li data-target="#about-carousel" data-slide-to="1"></li>
+									<li data-target="#about-carousel" data-slide-to="2"></li>
+								</ol>
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<img src="assets/img/about/image1.jpeg" class="img-responsive"  alt="About-Slide">
+									</div>
+									<div class="item">
+										<img src="assets/img/about/image2.jpeg" class="img-responsive"  alt="About-Slide">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- / END ABOUT SLIDE DESIGN AREA -->
+					
+					<!-- START SKILLS DESIGN AREA -->
+					<div class="col-md-4 col-sm-6 wow animated fadeInUp" data-wow-delay="0.60s">
+						<div class="skills-progress">
+							<h3>My Skills</h3>
+							<div class="progress">
+								<div class="lead">html5</div>
+								<div data-wow-delay="1s" data-wow-duration="1.2s" style="width: 45%; visibility: visible; animation-duration: 1.2s; animation-delay: 1s; animation-name: fadeInLeft;" data-progress="45%" class="progress-bar wow fadeInLeft">
+								</div>
+							</div>
+							<div class="progress">
+								<div class="lead">Python</div>
+								<div data-wow-delay="1s" data-wow-duration="1.2s" style="width: 30%; visibility: visible; animation-duration: 1.2s; animation-delay: 1s; animation-name: fadeInLeft;" data-progress="30%" class="progress-bar wow fadeInLeft">
+								</div>
+							</div>
+
+						</div>							
+					</div>
+					<!-- / END SKILLS DESIGN AREA -->
+					
+					<!-- START ABOUT INFO DESIGN AREA -->
+					<div class="col-md-4 col-sm-6 wow animated fadeInUp" data-wow-delay="0.70s">
+						<div class="about-info">
+							<h3>My Info</h3>
+							<ul class="list-unstyled">
+								<li><span>name:</span>Sofiat Ayobami Bello</li>
+								<li><span>Birthday:</span>October 28, 2000</li>
+								<li><span>address:</span>2, Ogeti street , Jibowu, Yaba,Lagos state.</li>
+								<li><span>phone:</span>+2348179030542</li>
+								<li><span>email:</span>sofiatbello15@gmail.com</li>
+							</ul>
+						</div>
+					</div>
+					<!-- / END ABOUT INFO DESIGN AREA -->
+
+				</div>
+			</div>
+		</section>
+		<!-- / END ABOUT ME DESIGN AREA -->
+	
+		<!-- START FOOTER DESIGN AREA -->
+		<footer class="copyright text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="wow animated bounceIn" data-wow-delay="0.60s">Sofiat &copy; 2022 All Rights Reserved.</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- / END FOOTER DESIGN AREA -->
+		 
+		<!-- All jQuery -->
+		<!-- JQUERY LATEST VERSION -->
+		<script src="assets/js/jquery-1.12.4.min.js"></script>
+        <!-- BOOTSTRAP JS-->
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		<!-- MIXITUP JS -->   
+		<script src="assets/js/jquery.mixitup.min.js"></script>
+        <!-- INVIEW JS -->
+		<script src="assets/js/jquery.inview.min.js"></script>
+		<!-- VENOBOX JS -->
+        <script src="assets/js/venobox.min.js"></script>
+		<!-- TYPED JS -->
+		<script src="assets/js/typed.min.js"></script>
+		<!-- MAGNIFICANT JS -->
+		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<!-- SWITCHER JS -->
+		<script src="assets/js/switcher.js"></script>
+        <!-- WOW JS -->
+        <script src="assets/js/wow.js"></script>
+		<!-- CONTACT FORM JS -->
+		<script src="assets/js/form-contact.js"></script>
+        <!-- JQUERY MAIN SCRIPTS -->
+        <script src="assets/js/scripts.js"></script>
+	</body>
+
+<!-- Mirrored from demo.wpxpress.net/persona/persona/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 17 Aug 2021 16:24:53 GMT -->
+</html>
